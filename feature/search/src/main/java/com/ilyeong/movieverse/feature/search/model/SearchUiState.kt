0dc1ed3b31671c73797d516c2ee8f783c0f94e3a
천일영 <dlfdud0304@naver.com>
2026-05-12 17:09:57 +0900

    #81 SearchUiState internal 적용

--- feature/search/src/main/java/com/ilyeong/movieverse/feature/search/model/SearchUiState.kt
+++ feature/search/src/main/java/com/ilyeong/movieverse/feature/search/model/SearchUiState.kt
@@ -2,7 +2,7 @@ package com.ilyeong.movieverse.feature.search.model
 
 import com.ilyeong.movieverse.core.model.Movie
 
-data class SearchUiState(
+internal data class SearchUiState(
     val isLoading: Boolean = false,
     val isFailure: Boolean = false,
     val trendMovieList: List<Movie> = emptyList(),
