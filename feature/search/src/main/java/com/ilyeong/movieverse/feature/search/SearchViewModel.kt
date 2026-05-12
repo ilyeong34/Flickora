09568c119bbddfe47fab35290fd0f7babbb7fb21
천일영 <dlfdud0304@naver.com>
2026-05-12 15:13:00 +0900

    #81 바뀐 UiState에 맞게 코드 변경

--- feature/search/src/main/java/com/ilyeong/movieverse/feature/search/SearchViewModel.kt
+++ feature/search/src/main/java/com/ilyeong/movieverse/feature/search/SearchViewModel.kt
@@ -2,26 +2,23 @@ package com.ilyeong.movieverse.feature.search
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import com.ilyeong.movieverse.core.data.movie.repository.MovieRepository
-import com.ilyeong.movieverse.core.model.Movie
 import com.ilyeong.movieverse.core.model.TimeWindow
 import com.ilyeong.movieverse.feature.search.model.SearchUiState
-import com.ilyeong.movieverse.feature.search.model.SearchUiState.Failure
-import com.ilyeong.movieverse.feature.search.model.SearchUiState.Loading
-import com.ilyeong.movieverse.feature.search.model.SearchUiState.Success
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.flatMapLatest
-import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
@@ -29,39 +26,39 @@ internal class SearchViewModel @Inject constructor(
     private val movieRepository: MovieRepository
 ) : ViewModel() {
 
-    private val _uiState = MutableStateFlow<SearchUiState>(Loading)
+    private val _uiState = MutableStateFlow(SearchUiState())
     val uiState = _uiState.asStateFlow()
 
-    private val _query = MutableStateFlow("")
-
     @OptIn(ExperimentalCoroutinesApi::class)
-    val searchMoviePaging = _query
-        .flatMapLatest {
-            when (it.isBlank()) {
-                true -> flowOf(PagingData.empty<Movie>())
-                false -> movieRepository.searchMoviePaging(it)
-            }
-        }
+    val searchMoviePaging = _uiState
+        .map { it.query }
+        .distinctUntilChanged()
+        .flatMapLatest { movieRepository.searchMoviePaging(it) }
         .cachedIn(viewModelScope)
 
     init {
         movieRepository.getTrendingMovieList(TimeWindow.DAY)
             .onStart {
-                _uiState.value = Loading
+                _uiState.update { it.copy(isLoading = true) }
                 delay(1000L)    // Loading Test
             }
-            .onEach {
-                when (it.isEmpty()) {
-                    true -> _uiState.value = Failure
-                    false -> _uiState.value = Success(it)
+            .onEach { trendMovieList ->
+                when (trendMovieList.isEmpty()) {
+                    true -> _uiState.update { it.copy(isLoading = false, isFailure = true) }
+                    false -> _uiState.update {
+                        it.copy(
+                            isLoading = false,
+                            trendMovieList = trendMovieList
+                        )
+                    }
                 }
             }
-            .catch { _uiState.value = Failure }
+            .catch { _uiState.update { it.copy(isLoading = false, isFailure = true) } }
             .launchIn(viewModelScope)
     }
 
 
     fun setQuery(query: String) {
-        _query.value = query
+        _uiState.update { it.copy(query = query) }
     }
 }
