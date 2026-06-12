44e31055d512e997464a2fad1eee1836c8d7e5f1
천일영 <dlfdud0304@naver.com>
2026-06-12 15:46:30 +0900

    #88 isGuest를 Preference로 저장하는 로직 구현

--- core/datastore/datastore-user/src/main/java/com/ilyeong/movieverse/core/datastore/user/UserPreferenceDataSource.kt
+++ core/datastore/datastore-user/src/main/java/com/ilyeong/movieverse/core/datastore/user/UserPreferenceDataSource.kt
@@ -3,4 +3,6 @@ package com.ilyeong.movieverse.core.datastore.user
 interface UserPreferenceDataSource {
     suspend fun getSessionId(): String
     suspend fun saveSessionId(sessionId: String)
+    suspend fun isGuestMode(): Boolean
+    suspend fun saveGuestMode(isGuest: Boolean)
 }
